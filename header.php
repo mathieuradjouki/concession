908497b41a2ed9475bf643b4a80bffc046daff56
lisaelyacoubi <lisa.elyacoubi@gmail.com>
2020-06-18 11:15:20 +0200

    [done] header

--- header.php
+++ header.php
@@ -6,4 +6,18 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
   </head>
   <body>
-  
+    <!-- Start Header -->
+    <header class="image-header">
+      <!-- Navbar -->
+      <nav class="navbar navbar-expand-lg navbar-light opacity">
+        <span><img src="images/header/hamburger-icon.png" alt="hamburger-icon" class="hamburger-icon"></span>
+        <div class="logo text-center mt-4">
+          <a href="#"><img src="images/header/logo.png" alt="logo"></a>
+        </div>
+      </nav>
+      <!-- End navbar -->
+      <div class="d-flex justify-content-end">
+        <h2 class="text-uppercase text-white pr-2">notre expertise</h2>
+      </div>
+    </header>
+    <!-- End Header -->

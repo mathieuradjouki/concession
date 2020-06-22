aa7743f7dff8529f5eed5ea7763fff1ed39eb8df
mathieuradjouki <m.radjouki@codeur.online>
2020-06-22 15:48:38 +0200

    page service desktop

--- header.php
+++ header.php
@@ -4,20 +4,35 @@
     <meta charset="utf-8">
     <title>Sport design service</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="service.css">
+    <link rel="stylesheet" href="main.css">
   </head>
   <body>
     <!-- Start Header -->
-    <header class="image-header">
-      <!-- Navbar -->
-      <nav class="navbar navbar-expand-lg navbar-light opacity">
-        <span><img src="images/header/hamburger-icon.png" alt="hamburger-icon" class="hamburger-icon"></span>
-        <div class="logo text-center mt-4">
-          <a href="#"><img src="images/header/logo.png" alt="logo"></a>
+      <header class="header-service">
+        <div class="container-fluid">
+<div class="box">
+          <div class="box-gauche col-6 p-3">
+            <h2 class="h2-services">QUE PEUT-ON FAIRE POUR VOUS?</h2>
+            <p>Sport Design Automobiles est à votre écoute pour la recherche de votre futur véhicule.<br>Nos fournisseurs (français et étrangés) nous proposent une large gamme de véhicules neufs et occasions, de quoi nous permettre de vous trouver LE véhicule qui vous convient.</p>
+            <p>Les délais de livraison sont de 10 à 15 jours. Le temps pour nous de rapatrier le véhicule dans nos locaux et de l’immatriculer.</p>
+            <p>Vous ne vous occupez de rien ! Comme en concession le véhicule vous est livré clé en main avec une garantie.</p>
+            <p>Si votre emploi du temps ne vous permet pas de faire le déplacement, nous avons également la possibilité de vous livrer le véhicule (à vos frais) à votre domicile par un transporteur.</p>
+            <p>Nous espérons vous compter très vite dans nos fidèles clients !</p>
+          </div>
+
+
+          <div class="domaines col-5 p-3">
+            <h2 class="h2-services">NOS DOMAINES DE COMPTENCE</h2>
+              <p>Véhicules neufs toute marque</p>
+              <p>Véhicules occasions toutes marque</p>
+              <p>Financement</p>
+              <p>Garantie</p>
+              <p>Reprises</p>
+          </div>
         </div>
-      </nav>
-      <!-- End navbar -->
-      <div class="d-flex justify-content-end">
-        <h2 class="text-uppercase text-white pr-2">notre expertise</h2>
-      </div>
-    </header>
-    <!-- End Header -->
+
+        </div>
+      </header>
+
+    <!--End header-->

222d1ef99fa287bd0ffc344ce00773b5a6267e3c
mathieuradjouki <m.radjouki@codeur.online>
2020-06-23 10:34:58 +0200

    ajout services.php

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html lang=fr dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Sport design</title>
+    <title>Sport design service</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <!-- CSS THEME -->
     <link rel="stylesheet" href="main.css">
@@ -20,25 +20,3 @@
       <!-- End navbar -->
     </header>
     <!-- End Header -->
-    <!DOCTYPE html>
-    <html lang=fr dir="ltr">
-      <head>
-        <meta charset="utf-8">
-        <title>Sport design service</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-        <!-- CSS THEME -->
-        <link rel="stylesheet" href="main.css">
-      </head>
-      <body>
-        <!-- Start Header -->
-        <header class="image-header">
-          <!-- Navbar -->
-          <nav class="navbar navbar-expand-lg navbar-light opacity">
-            <span><img src="images/header/hamburger-icon.png" alt="hamburger-icon" class="hamburger-icon"></span>
-            <div class="logo text-center mt-4">
-              <a href="#"><img src="images/header/logo.png" alt="logo"></a>
-            </div>
-          </nav>
-          <!-- End navbar -->
-        </header>
-        <!-- End Header -->

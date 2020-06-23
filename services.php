779b44631c5ea916c25b4101a713c4b5f29b06b0
mathieuradjouki <m.radjouki@codeur.online>
2020-06-23 13:54:16 +0200

    changement de titre

--- services.php
+++ services.php
@@ -7,24 +7,24 @@ include('header.php');
   <!-- Start Header -->
     <header class="header-service">
       <div class="container-fluid">
-<div class="box">
-        <div class="box-gauche col-6 p-3">
-          <h2 class="h2-services">QUE PEUT-ON FAIRE POUR VOUS?</h2>
-          <p>Sport Design Automobiles est à votre écoute pour la recherche de votre futur véhicule.<br>Nos fournisseurs (français et étrangés) nous proposent une large gamme de véhicules neufs et occasions, de quoi nous permettre de vous trouver LE véhicule qui vous convient.</p>
-          <p>Les délais de livraison sont de 10 à 15 jours. Le temps pour nous de rapatrier le véhicule dans nos locaux et de l’immatriculer.</p>
-          <p>Vous ne vous occupez de rien ! Comme en concession le véhicule vous est livré clé en main avec une garantie.</p>
-          <p>Si votre emploi du temps ne vous permet pas de faire le déplacement, nous avons également la possibilité de vous livrer le véhicule (à vos frais) à votre domicile par un transporteur.</p>
-          <p>Nous espérons vous compter très vite dans nos fidèles clients !</p>
+        <div class="box">
+          <div class="box-gauche col-6 p-3">
+            <h2 class="h2-services">QUE PEUT-ON FAIRE POUR VOUS?</h2>
+            <p>Sport Design Automobiles est à votre écoute pour la recherche de votre futur véhicule.<br>Nos fournisseurs (français et étrangés) nous proposent une large gamme de véhicules neufs et occasions, de quoi nous permettre de vous trouver LE véhicule qui vous convient.</p>
+            <p>Les délais de livraison sont de 10 à 15 jours. Le temps pour nous de rapatrier le véhicule dans nos locaux et de l’immatriculer.</p>
+            <p>Vous ne vous occupez de rien ! Comme en concession le véhicule vous est livré clé en main avec une garantie.</p>
+            <p>Si votre emploi du temps ne vous permet pas de faire le déplacement, nous avons également la possibilité de vous livrer le véhicule (à vos frais) à votre domicile par un transporteur.</p>
+            <p>Nous espérons vous compter très vite dans nos fidèles clients !</p>
+          </div>
+          <div class="domaines col-5 p-3">
+            <h2 class="h2-services">NOS DOMAINES DE COMPTENCE</h2>
+              <p>Véhicules neufs toute marque</p>
+              <p>Véhicules occasions toutes marque</p>
+              <p>Financement</p>
+              <p>Garantie</p>
+              <p>Reprises</p>
+          </div>
         </div>
-        <div class="domaines col-5 p-3">
-          <h2 class="h2-services">NOS DOMAINES DE COMPTENCE</h2>
-            <p>Véhicules neufs toute marque</p>
-            <p>Véhicules occasions toutes marque</p>
-            <p>Financement</p>
-            <p>Garantie</p>
-            <p>Reprises</p>
-        </div>
-      </div>
       </div>
     </header>
 <!--section sport design-->

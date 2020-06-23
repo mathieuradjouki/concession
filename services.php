74f178477621addb760341b28c96ec1ea749981d
mathieuradjouki <m.radjouki@codeur.online>
2020-06-23 14:53:03 +0200

    modification class service.php et service.css

--- services.php
+++ services.php
@@ -7,40 +7,40 @@ include('header.php');
   <!-- Start Header -->
     <header class="header-service">
       <div class="container-fluid">
-        <div class="box">
-          <div class="box-gauche col-6 p-3">
+        <div class="box-services">
+          <div class="box-gauche-services col-6 p-3">
             <h2 class="h2-services">QUE PEUT-ON FAIRE POUR VOUS?</h2>
-            <p>Sport Design Automobiles est à votre écoute pour la recherche de votre futur véhicule.<br>Nos fournisseurs (français et étrangés) nous proposent une large gamme de véhicules neufs et occasions, de quoi nous permettre de vous trouver LE véhicule qui vous convient.</p>
-            <p>Les délais de livraison sont de 10 à 15 jours. Le temps pour nous de rapatrier le véhicule dans nos locaux et de l’immatriculer.</p>
-            <p>Vous ne vous occupez de rien ! Comme en concession le véhicule vous est livré clé en main avec une garantie.</p>
-            <p>Si votre emploi du temps ne vous permet pas de faire le déplacement, nous avons également la possibilité de vous livrer le véhicule (à vos frais) à votre domicile par un transporteur.</p>
-            <p>Nous espérons vous compter très vite dans nos fidèles clients !</p>
+            <p class="p-services">Sport Design Automobiles est à votre écoute pour la recherche de votre futur véhicule.<br>Nos fournisseurs (français et étrangés) nous proposent une large gamme de véhicules neufs et occasions, de quoi nous permettre de vous trouver LE véhicule qui vous convient.</p>
+            <p class="p-services">Les délais de livraison sont de 10 à 15 jours. Le temps pour nous de rapatrier le véhicule dans nos locaux et de l’immatriculer.</p>
+            <p class="p-services">Vous ne vous occupez de rien ! Comme en concession le véhicule vous est livré clé en main avec une garantie.</p>
+            <p class="p-services">Si votre emploi du temps ne vous permet pas de faire le déplacement, nous avons également la possibilité de vous livrer le véhicule (à vos frais) à votre domicile par un transporteur.</p>
+            <p class="p-services">Nous espérons vous compter très vite dans nos fidèles clients !</p>
           </div>
-          <div class="domaines col-5 p-3">
+          <div class="domaines-services col-5 p-3">
             <h2 class="h2-services">NOS DOMAINES DE COMPTENCE</h2>
-              <p>Véhicules neufs toute marque</p>
-              <p>Véhicules occasions toutes marque</p>
-              <p>Financement</p>
-              <p>Garantie</p>
-              <p>Reprises</p>
+              <p class="p-services">Véhicules neufs toute marque</p>
+              <p class="p-services">Véhicules occasions toutes marque</p>
+              <p class="p-services">Financement</p>
+              <p class="p-services">Garantie</p>
+              <p class="p-services">Reprises</p>
           </div>
         </div>
       </div>
     </header>
 <!--section sport design-->
-  <section class="sport-design">
+  <section class="sport-design-services">
     <div class="container-fluid">
-      <div class="row row-sport">
+      <div class="row row-sport-servives">
         <h2 class="h2-services col-lg-6">SPORT DESIGN AUTOMOBILES, C'EST AUSSI...</h2>
-        <img src="image/line-sport-compressor.png" alt="img" class="col-lg-6 line-sport img-fluid">
+        <img src="image/line-sport-compressor.png" alt="img" class="col-lg-6 line-sport-services img-fluid">
       </div>
-      <div class="container sport-design-card">
+      <div class="container sport-design-card-services">
         <div class="row row-cols-1 row-cols-md-2">
           <div class="col mb-4">
             <div class="card card-services h-100">
               <div class="card-body">
                 <h5 class="h5-services">SERVICE D'IMMATRICULATION</h5>
-                <p class="card-text-services p-2">Votre carte grise (hors première immatriculation d'un véhicule étranger) en 15 minutes.</p>
+                <p class="card-text-services p-services p-2">Votre carte grise (hors première immatriculation d'un véhicule étranger) en 15 minutes.</p>
               </div>
             </div>
           </div>
@@ -48,7 +48,7 @@ include('header.php');
             <div class="card card-services h-100">
               <div class="card-body">
                 <h5 class="h5-services">GARANTIE SERENITE</h5>
-                <p class="card-text-services p-2">Chaque véhicule d'occasion est vendu avec une garantie pouvant aller de 3 à 12 mois.</p>
+                <p class="card-text-services p-services p-2">Chaque véhicule d'occasion est vendu avec une garantie pouvant aller de 3 à 12 mois.</p>
               </div>
             </div>
           </div>
@@ -56,7 +56,7 @@ include('header.php');
             <div class="card card-services h-100">
               <div class="card-body">
                 <h5 class="h5-services">FINANCEMENT</h5>
-                <p class="card-text-services p-2">Pour l'achat de tout véhicule, nous vous proposons le financement de celui-ci.</p>
+                <p class="card-text-services p-services p-2">Pour l'achat de tout véhicule, nous vous proposons le financement de celui-ci.</p>
               </div>
             </div>
           </div>
@@ -64,7 +64,7 @@ include('header.php');
             <div class="card card-services h-100">
               <div class="card-body">
                 <h5 class="h5-services">ESSAI GRATUIT</h5>
-                <p class="card-text-services p-2">Tous les véhicules en stock ont la possibilité d’être essayés.</p>
+                <p class="card-text-services p-services p-2">Tous les véhicules en stock ont la possibilité d’être essayés.</p>
               </div>
             </div>
           </div>
@@ -78,7 +78,7 @@ include('header.php');
     <div class="container-fluid">
       <div class="row row-utilities">
         <h2 class="h2-services col-md-3">ON S'OCCUPE DE VOUS!</h2>
-        <img src="image/maquette-sport-design.png" alt="line-white" class="col-md-9 img-fluid line-sport">
+        <img src="image/maquette-sport-design.png" alt="line-white" class="col-md-9 img-fluid line-sport-services">
       </div>
         <div class="container utilities-card">
           <div class="row row-cols-1 row-cols-md-3">
@@ -87,7 +87,7 @@ include('header.php');
                 <img src="image/mover-truck-compressor.png" alt="truck" class="img-utilities">
                 <div class="card-body">
                   <h5 class="h5-utilities">LIVRAISON A DOMICILE</h5>
-                  <p class="card-text-utilities">Nous vous proposons la livraison de votre véhicule à domicile, si votre emploi du temps ne vous permet pas de vous déplacer.</p>
+                  <p class="card-text-utilities p-services">Nous vous proposons la livraison de votre véhicule à domicile, si votre emploi du temps ne vous permet pas de vous déplacer.</p>
                 </div>
               </div>
             </div>
@@ -96,7 +96,7 @@ include('header.php');
                 <img src="image/train-compressor.png" alt="train" class="img-utilities">
                 <div class="card-body">
                   <h5 class="h5-utilities">NOUS VOUS CHERCHONS A LA GARE</h5>
-                  <p class="card-text-utilities">Vous arrivez en train? Nous nous proposons de venir vous chercher en gare de Colmar.</p>
+                  <p class="card-text-utilities p-services">Vous arrivez en train? Nous nous proposons de venir vous chercher en gare de Colmar.</p>
                 </div>
               </div>
             </div>
@@ -105,7 +105,7 @@ include('header.php');
                 <img src="image/plane-compressor.png" alt="plane" class="img-utilities">
                 <div class="card-body">
                   <h5 class="h5-utilities">NOUS VOUS CHERCHONS A L'AEROPORT</h5>
-                  <p class="card-text-utilities">Vous arrivez en avion? Nous vous proposons de venir vous chercher à l'aéroport Bâle-Mulhouse.</p>
+                  <p class="card-text-utilities p-services">Vous arrivez en avion? Nous vous proposons de venir vous chercher à l'aéroport Bâle-Mulhouse.</p>
                 </div>
               </div>
             </div>
@@ -114,7 +114,7 @@ include('header.php');
                 <img src="image/bed-compressor.png" alt="bed" class="img-utilities">
                 <div class="card-body">
                   <h5 class="h5-utilities">HOTEL A PROXIMITE</h5>
-                  <p class="card-text-utilities">Vous avez fait un long trajet pour nous rejoindre? Ne prenez pas la route avant une bonne nuit de sommeil!Plusieurs hôtels se trouvent à proximité de nos locaux.</p>
+                  <p class="card-text-utilities p-services">Vous avez fait un long trajet pour nous rejoindre? Ne prenez pas la route avant une bonne nuit de sommeil!Plusieurs hôtels se trouvent à proximité de nos locaux.</p>
                 </div>
               </div>
             </div>
@@ -123,7 +123,7 @@ include('header.php');
                 <img src="image/kitchen-compressor.png" alt="kitchen" class="img-utilities">
                 <div class="card-body">
                   <h5 class="h5-utilities">FACILITE D'ACCES</h5>
-                  <p class="card-text-utilities">Nous nous situons juste à côté de l'autoroute. Idéal pour nous rejoindre.</p>
+                  <p class="card-text-utilities p-services">Nous nous situons juste à côté de l'autoroute. Idéal pour nous rejoindre.</p>
                 </div>
               </div>
             </div>
@@ -132,7 +132,7 @@ include('header.php');
                 <img src="image/road-compressor.png" alt="road" class="img-utilities">
                 <div class="card-body">
                   <h5 class="h5-utilities">LIVRAISON A DOMICILE</h5>
-                  <p class="card-text-utilities">Nous vous proposons la livraison de votre véhicule à domicile, si votre emploi du temps ne vous permet pas de vous déplacer.</p>
+                  <p class="card-text-utilities p-services">Nous vous proposons la livraison de votre véhicule à domicile, si votre emploi du temps ne vous permet pas de vous déplacer.</p>
                 </div>
               </div>
             </div>
